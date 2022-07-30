2575b315d0f37ace4d1c5191c24dd8003e72ff48
MicaiahReid <micaiahreid@gmail.com>
2022-07-30 13:38:52 -0400

    finish changing over types oopps

--- src/chains/ethereum/ethereum/src/blockchain.ts
+++ src/chains/ethereum/ethereum/src/blockchain.ts
@@ -1068,7 +1068,7 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
   public async createAccessList(
     transaction: SimulationTransaction,
     simulationBlock: Block
-  ): Promise<{ accessList: AccessList; gasUsed: string }> {
+  ): Promise<CreateAccessListResult> {
     const common = this.fallback
       ? this.fallback.getCommonForBlockNumber(
           this.common,
